1a446c5550b9513837e0ebee78c96d3b7110760f
Divine <ifeanyidivineonuoha@gmail.com>
2024-04-08 07:53:36 +0100

    implemented the accordion component

--- build/index.js
+++ build/index.js
@@ -1,2 +1,56 @@
 "use strict";
-console.log('Hello World');
+class Accordion {
+    constructor(elem) {
+        this.accordion = elem;
+        this.buttons = elem.querySelectorAll('button');
+        elem.addEventListener('click', this.onClickEvent.bind(this));
+        elem.addEventListener('keydown', this.onKeyDownEvent.bind(this));
+    }
+    onClickEvent({ target }) {
+        let btn = target.closest('button');
+        if (!btn)
+            return;
+        this.togglePanel(btn);
+    }
+    onKeyDownEvent(event) {
+        let target = event.target;
+        if (target.tagName != 'BUTTON')
+            return;
+        if (event.code == 'ArrowDown') {
+            this.onArrowDown(target);
+        }
+        else if (event.code == 'ArrowUp') {
+            this.onArrowUp(target);
+        }
+    }
+    onArrowDown(target) {
+        let id = Number(target.dataset.id);
+        if (id == this.buttons.length) {
+            id = 0;
+        }
+        let nextBtn = this.accordion.querySelector(`[data-id="${++id}"]`);
+        nextBtn.focus();
+    }
+    onArrowUp(target) {
+        let id = Number(target.dataset.id);
+        if (id == 1) {
+            id = this.buttons.length + 1;
+        }
+        let prevBtn = this.accordion.querySelector(`[data-id="${--id}"]`);
+        prevBtn.focus();
+    }
+    togglePanel(btn) {
+        if (btn.getAttribute('aria-expanded') == 'false') {
+            btn.setAttribute('aria-expanded', 'true');
+            btn.closest('.accordion__faq').classList.add('open');
+        }
+        else {
+            btn.setAttribute('aria-expanded', 'false');
+            btn.closest('.accordion__faq').classList.remove('open');
+        }
+    }
+}
+document.addEventListener('DOMContentLoaded', () => {
+    const elem = document.querySelector('.accordion');
+    new Accordion(elem);
+});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,62 @@
-console.log('Hello World');
+class Accordion {
+  accordion: HTMLDivElement;
+  buttons: NodeListOf<HTMLButtonElement>;
+
+  constructor(elem: HTMLDivElement) {
+    this.accordion = elem;
+    this.buttons = <NodeListOf<HTMLButtonElement>>elem.querySelectorAll('button');
+
+    elem.addEventListener('click', this.onClickEvent.bind(this));
+    elem.addEventListener('keydown', this.onKeyDownEvent.bind(this));
+  }
+
+  onClickEvent({target}: MouseEvent) {
+    let btn = (<HTMLElement>target).closest('button');
+    if (!btn) return;
+    this.togglePanel(btn);
+  }
+
+  onKeyDownEvent(event: KeyboardEvent) {
+    let target = <HTMLButtonElement>event.target;
+    if (target.tagName != 'BUTTON') return;
+
+    if (event.code == 'ArrowDown') {
+      this.onArrowDown(target);
+    } else if (event.code == 'ArrowUp') {
+      this.onArrowUp(target);
+    }
+  }
+
+  onArrowDown(target: HTMLButtonElement) {
+    let id = Number(target.dataset.id);
+    if (id == this.buttons.length) {
+      id = 0;
+    }
+    let nextBtn = <HTMLButtonElement>this.accordion.querySelector(`[data-id="${++id}"]`);
+    nextBtn.focus();
+  }
+
+  onArrowUp(target: HTMLButtonElement) {
+    let id = Number(target.dataset.id);
+    if (id == 1) {
+      id = this.buttons.length + 1;
+    }
+    let prevBtn = <HTMLButtonElement>this.accordion.querySelector(`[data-id="${--id}"]`);
+    prevBtn.focus();
+  }
+
+  togglePanel(btn: HTMLButtonElement) {
+    if (btn.getAttribute('aria-expanded') == 'false') {
+      btn.setAttribute('aria-expanded', 'true');
+      (<HTMLElement>btn.closest('.accordion__faq')).classList.add('open');
+    } else {
+      btn.setAttribute('aria-expanded', 'false');
+      (<HTMLElement>btn.closest('.accordion__faq')).classList.remove('open');
+    }
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const elem = <HTMLDivElement>document.querySelector('.accordion');
+  new Accordion(elem);
+});
